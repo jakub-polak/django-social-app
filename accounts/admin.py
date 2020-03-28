fec96bcf51785148ef97cdb408716d61ba0c3298
Jakub Polak <jakubpolak95@gmail.com>
2020-03-28 21:07:01 +0100

    Add UserProfileAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 
 from accounts.models import UserProfile
 
-admin.site.register(UserProfile)
+admin.site.site_header = 'Administration'
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_info', 'city', 'website', 'phone')
+
+    def user_info(self, obj: UserProfile):
+        return obj.description
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request).order_by('-phone', 'user')
+        return queryset
+
+    user_info.short_description = 'info'
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
